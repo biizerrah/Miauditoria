af44e30dee82684ad22e8414fbe597bbdcadd0d7
biizerrah <biizerrah@outlook.com>
2025-11-19 20:47:36 -0300

    feat(delete): implement function to remove a single cat record

---
+++ project/remover.py
@@ -0,0 +1,17 @@
+from cadastrar import todos_os_gatos
+
+def remover(removendo):
+
+    for gatos in todos_os_gatos.values():
+
+
+        for gato in gatos:
+            if removendo == gato[0]:
+
+
+                gatos.remove(gato)
+                print(f"Gatinho '{removendo}' removido(a) com sucesso!")
+                return
+
+
+    print('Gatinho não encontrado')
